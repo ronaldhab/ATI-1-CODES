903ce8b37ccba4f5a5a405416bc219cd63bcb29c
Ronald Herrera <ronald.alfredo.hb@gmail.com>
2025-11-16 14:32:43 -0400

    configuración desde javascript

---
+++ js/index.js
@@ -0,0 +1,53 @@
+const [nombre, siglas, periodo] = config['sitio'];
+tituloIndex = nombre + siglas + ' ' + periodo;
+
+const contactoCompleto = config['email'].split(/(?=\[email\])/);
+let textoContacto = contactoCompleto[0];
+let enlaceContacto = contactoCompleto[1];
+
+// Index.html conf
+let nombrePag = document.getElementById('nombre-pag');
+let siglasUCV = document.getElementsByClassName('logo-texto-UCV')[0];
+let periodoTitulo = document.getElementById('periodo');
+let saludo = document.getElementById('saludo');
+let copyright = document.getElementById('copyright');
+let textoNombreForm = document.getElementById('input-nombre');
+let textoBtn = document.getElementsByClassName('btn-buscar')[0];
+
+// Perfil.html conf
+let nombreEstudiante = document.getElementsByClassName('estudiante-nombre')[0];
+let descipcionEstudiante = document.getElementsByClassName(
+  'estudiante-descipcion'
+)[0];
+let color = document.getElementById('color');
+let musica = document.getElementById('musica');
+let libro = document.getElementById('libro');
+let videojuego = document.getElementById('videojuego');
+let lenguajes = document.getElementById('lenguajes');
+let contacto = document.getElementById('texto-contacto');
+let enlace = document.getElementsByClassName('contacto-enlace')[0];
+
+/******Validaciones********/
+// Index.html
+nombrePag && (nombrePag.innerHTML = nombre);
+nombrePag && (document.title = tituloIndex);
+siglasUCV && (siglasUCV.innerHTML = siglas);
+periodoTitulo && (periodoTitulo.innerHTML = periodo);
+saludo && (saludo.innerHTML = config['saludo']);
+copyright && (copyright.innerHTML = config['copyRight']);
+textoNombreForm && (textoNombreForm.placeholder = config['nombre'] + '...');
+textoBtn && (textoBtn.value = config['buscar']);
+
+// Perfil.html
+nombreEstudiante && (nombreEstudiante.innerHTML = config['nombre']);
+descipcionEstudiante &&
+  (descipcionEstudiante.innerHTML = config['descripcion']);
+color && (color.innerHTML = config['color']);
+musica && (musica.innerHTML = config['musica']);
+libro && (libro.innerHTML = config['libro']);
+videojuego && (videojuego.innerHTML = config['video_juego']);
+lenguajes && (lenguajes.innerHTML = config['lenguajes']);
+contacto && (contacto.innerHTML = textoContacto);
+enlace && (enlace.innerHTML = enlaceContacto);
+
+/****************************/
